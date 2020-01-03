d81a3a5c7ba2a4bf5e0cd034fa17d91239723ce1
alinepazz <aline.soares.2@hotmail.com>
2020-01-03 16:57:59 -0300

    Acrescentando informações sobre mocha

--- 05-tests/01/service.js
+++ 05-tests/01/service.js
@@ -1,15 +1,20 @@
 const {
     get
-} = require('axios')
+} = require('axios') //aqui estou consumindo a API Star Wars
 
-const URL = `https://swapi.co/api/people`
+
+const URL = `https://swapi.co/api/people` // consumindo o serviço
 
 async function obterPessoas(nome) {
-    const url = `${URL}/?search=${nome}&format=json`
+    const url = `${URL}/?search=${nome}&format=json` // aqui vou pesquisar pelo nome e quero o formato json
+
     const result =  await get(url)
-    return result.data.results.map(mapearPessoas)
+    return result.data.results.map(mapearPessoas)// pra cada item do results me traz somente o que especifiquei em mapearPessoas
+    
 }
-function mapearPessoas (item) {
+function mapearPessoas (item) { // mapear as informações que vir da Api
+   // do resultado que vir quero somente o nome e o peso
+
    return {
        nome: item.name,
        peso: item.height
--- 05-tests/01/tests.js
+++ 05-tests/01/tests.js
@@ -1,15 +1,20 @@
-const assert = require('assert')
+const assert = require('assert') // trabalhando com asserções dentro do js
+//para trabalhar de forma automatizada com esse fremework-> estalamos de forma global-> npm install -g mocha
+//para o projeto entender que estou trabalhando com mocha, precisamos instalar
+//como dependência de desenvolvimento  -> --save-dev mocha
+// instalamos o Nock para simular resultados fakes -> npm install nock
 const {
     obterPessoas
 } = require('./service')
 
-// instalamos o pacote mocha, para simular requisições
-const nock = require('nock')
+// instalamos o pacote nock, para simular requisições
+const nock = require('nock') // estou trazendo para a memoria
 
 
-describe('Star Wars Tests', function () {
-    this.beforeAll(() => {
-        const response = 
+
+describe('Star Wars Tests', function () { //suíte de testes
+    this.beforeAll(() => { // antes de cada uma das funções, quero que execute uma tarefa
+        const response = // definir resposta
             {
                 count: 1,
                 next: null,
@@ -33,16 +38,16 @@ describe('Star Wars Tests', function () {
                   }
                 ]
               }
-            nock('https://swapi.co/api/people')  
-            .get('/?search=r2-d2&format=json')
-            .reply(200, response)
+            nock('https://swapi.co/api/people')  // toda vez que for passado essa url
+            .get('/?search=r2-d2&format=json') // com esses paramentros
+            .reply(200, response) // quero que responda com status 200 e o response que criamos
 
 
         } )
    
 
-    it('deve buscar o r2d2 com o formato correto', async () => {
-        const expected = [{
+    it('deve buscar o r2d2 com o formato correto', async () => {// definir os sub testes
+        const expected = [{ // estado esperado/ construir a assinatura
             nome: 'R2-D2', 
             peso: '96'
         }]
